25d9a653d0dce020a60883df825c792083bd71dc
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2025-03-28 04:15:44 +0700

    feat: add optional id field to user and session interfaces

--- frontend/types/index.ts
+++ frontend/types/index.ts
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
+      id?: string;
       name?: string | null;
       email?: string | null;
       image?: string | null;
@@ -12,5 +13,7 @@ declare module "next-auth" {
 
   interface User {
     token?: string;
-  }
+    id?: string;
+    name?: string | null;
+    email?: string | null;}
 }
